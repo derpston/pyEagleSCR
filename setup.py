c1736866bec751ec224661d3ffa675dbd6dec9d1
user <user@example.com>
2018-02-18 16:59:53 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
       name = 'EagleSCR'
-   ,  version = '0.1.0'
+   ,  version = '0.2.0'
    ,  description = 'Parser for the Eagle scripting language'
    ,  long_description = read_md('README.md')
    ,  author = 'Derp Ston'
