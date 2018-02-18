fbec0ae287a789312bf677b98ccb2ebd50d68107
user <user@example.com>
2018-02-18 00:33:25 +0000

    Using argparse in main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import sys
-import re
-import models
 import parser
 import pprint
+import argparse
 
-parser = parser.Parser()
+argparser = argparse.ArgumentParser(description="Parse an Eagle SCR file and "\
+    "display some basic information about it.")
+argparser.add_argument('path', metavar='path-to-scr-file', type=str, \
+    help="Path to an Eagle SCR file.")
+args = argparser.parse_args()
 
-for line_index, line in enumerate(open(sys.argv[1])):
-    if not parser.handle_line(line):
+# Eagle SCR parser.
+scrparser = parser.Parser()
+
+for line_index, line in enumerate(open(args.path)):
+    if not scrparser.handle_line(line):
         print "Unsupported command on line %d: %s" % (line_index, repr(line))
 
 print "Context:"
-pprint.pprint(parser.context)
+pprint.pprint(scrparser.context)
 
